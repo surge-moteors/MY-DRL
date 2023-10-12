778f139f849e4afd93b6f3acdcf87d118aa477d9
Ziteng_He <ziteng_he@bupt.edu.cn>
2023-10-12 12:15:09 +0800

    DQN_CartPole

--- pytorch/DQN/DQN_CarPole-v1.py
+++ pytorch/DQN/DQN_CarPole-v1.py
@@ -22,10 +22,10 @@ Transition = namedtuple('Transition', ['state', 'action', 'reward', 'next_state'
 
 
 class Net(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
         self.fc1 = nn.Linear(num_state, 100)
-        self.fc2 = nn.Linear(num_state, 100)
+        self.fc2 = nn.Linear(100, num_action)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -44,15 +44,15 @@ class DQN:
         super(DQN, self).__init__()
         self.capacity = 8000
         self.learning_rate = 1e-3
-        self. batch_size = 256
+        self.batch_size = 256
         self.gamma = 0.995
-        self.eval_net, self.target_net = Net()
+        self.eval_net, self.target_net = Net(), Net()
         self.memory = [None] * self.capacity
         self.memory_count = 0
         self.update_count = 0
         self.optimizer = optim.Adam(self.eval_net.parameters(), self.learning_rate)
         self.loss_func = nn.MSELoss()
-        self.writer = SummaryWriter('./DQN/logs')  # todo:
+        self.writer = SummaryWriter('./DQN/logs')
 
     def select_action(self, state):
         state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
@@ -73,23 +73,23 @@ class DQN:
         if self.memory_count >= self.capacity:
 
             # 这里使用了整个数据集
-            state = torch.tensor([t.state for t in self.memory]).float()
-            action = torch.LongTensor([t.action for t in self.memory]).view(-1, 1).long()
-            reward = torch.tensor([t.reward for t in self.memory]).float()
-            next_state = torch.tensor([t.next_state for t in self.memory]).float()
+            state = torch.tensor(np.array([t.state for t in self.memory])).float()
+            action = torch.LongTensor(np.array([t.action for t in self.memory])).view(-1, 1).long()
+            reward = torch.tensor(np.array([t.reward for t in self.memory])).float()
+            next_state = torch.tensor(np.array([t.next_state for t in self.memory])).float()
 
             reward = (reward - reward.mean()) / (reward.std() + 1e-7)
             with torch.no_grad():
                 target_v = reward + self.gamma * self.target_net(next_state).max(1)[0]
 
-            # todo:
             for index in BatchSampler(SubsetRandomSampler(range(len(self.memory))), batch_size=self.batch_size, drop_last=False):
+                # 能够让index 不重复地采样
                 eval_value = (self.eval_net(state).gather(1, action))[index]  # 求Q(s, a)
                 loss = self.loss_func(target_v[index].unsqueeze(1), eval_value)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                self.writer.add_scalar('loss/value_loss', loss, self.update_count)  # todo:
+                self.writer.add_scalar('loss/value_loss', loss, self.update_count)
                 self.update_count += 1
                 if self.update_count % 100 == 0:
                     self.target_net.load_state_dict(self.eval_net.state_dict())
@@ -101,7 +101,7 @@ def main():
     agent = DQN()
     for i_ep in range(num_episodes):
         state = env.reset()
-        for t in range(10000):  # todo: the function of 10000
+        for t in range(10000):
             action = agent.select_action(state)
             next_state, reward, done, info = env.step(action)
             if render:
